178226aef4818e62b56070dab50730e3a83458fc
n0r3f <info@noref.fr>
2025-02-25 01:16:21 +0100

    Remove obsolete Next.js build artifacts and update server pages manifest

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"dYxqOn9r/UCfsFDRsvuqX+MApSRVj8HFnP3mE4FFVaw=\"\n}"
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
 import { useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
   const [darkMode, setDarkMode] = useState(true);
   const [menuOpen, setMenuOpen] = useState(false);
+  const router = useRouter();
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
@@ -12,6 +14,11 @@ function MyApp({ Component, pageProps }) {
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
+  };
+
+  const handleLinkClick = (href) => {
+    setMenuOpen(false);
+    router.push(href);
   };
 
   return (
@@ -22,39 +29,17 @@ function MyApp({ Component, pageProps }) {
         {menuOpen && (
           <div className="menu">
             <div className="tile-list" style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gridGap: 0 }}>
-              <Link href="/seconnecter" legacyBehavior>
-                <a className="tile tile1" style={{ gridColumn: 'span 2' }}>Se connecter</a>
-              </Link>
-              <Link href="/logistique" legacyBehavior>
-                <a className="tile tile2" style={{ gridColumn: 'span 1' }}>Logistique</a>
-              </Link>
-              <Link href="/tracabilite" legacyBehavior>
-                <a className="tile tile3" style={{ gridColumn: 'span 1' }}>Traçabilité</a>
-              </Link>
-              <Link href="/production" legacyBehavior>
-                <a className="tile tile4" style={{ gridColumn: 'span 1' }}>Production</a>
-              </Link>
-              <Link href="/mediation" legacyBehavior>
-                <a className="tile tile5" style={{ gridColumn: 'span 1' }}>Médiation</a>
-              </Link>
-              <Link href="/sav" legacyBehavior>
-                <a className="tile tile6" style={{ gridColumn: 'span 1' }}>SAV</a>
-              </Link>
-              <Link href="/dons" legacyBehavior>
-                <a className="tile tile7" style={{ gridColumn: 'span 1' }}>Dons</a>
-              </Link>
-              <Link href="/bilans" legacyBehavior>
-                <a className="tile tile8" style={{ gridColumn: 'span 1' }}>Bilans</a>
-              </Link>
-              <Link href="/stocks" legacyBehavior>
-                <a className="tile tile9" style={{ gridColumn: 'span 1' }}>Stocks</a>
-              </Link>
-              <Link href="/admin" legacyBehavior>
-                <a className="tile tile10" style={{ gridColumn: 'span 1' }}>Accès admin</a>
-              </Link>
-              <Link href="/" legacyBehavior>
-                <a className="tile tile11" style={{ gridColumn: 'span 1' }}>Accueil</a>
-              </Link>
+              <a className="tile tile1" onClick={() => handleLinkClick('/seconnecter')} style={{ gridColumn: 'span 2' }}>Se connecter</a>
+              <a className="tile tile2" onClick={() => handleLinkClick('/logistique')} style={{ gridColumn: 'span 1' }}>Logistique</a>
+              <a className="tile tile3" onClick={() => handleLinkClick('/tracabilite')} style={{ gridColumn: 'span 1' }}>Traçabilité</a>
+              <a className="tile tile4" onClick={() => handleLinkClick('/production')} style={{ gridColumn: 'span 1' }}>Production</a>
+              <a className="tile tile5" onClick={() => handleLinkClick('/mediation')} style={{ gridColumn: 'span 1' }}>Médiation</a>
+              <a className="tile tile6" onClick={() => handleLinkClick('/sav')} style={{ gridColumn: 'span 1' }}>SAV</a>
+              <a className="tile tile7" onClick={() => handleLinkClick('/dons')} style={{ gridColumn: 'span 1' }}>Dons</a>
+              <a className="tile tile8" onClick={() => handleLinkClick('/bilans')} style={{ gridColumn: 'span 1' }}>Bilans</a>
+              <a className="tile tile9" onClick={() => handleLinkClick('/stocks')} style={{ gridColumn: 'span 1' }}>Stocks</a>
+              <a className="tile tile10" onClick={() => handleLinkClick('/admin')} style={{ gridColumn: 'span 1' }}>Accès admin</a>
+              <a className="tile tile11" onClick={() => handleLinkClick('/')} style={{ gridColumn: 'span 1' }}>Accueil</a>
             </div>
           </div>
         )}
